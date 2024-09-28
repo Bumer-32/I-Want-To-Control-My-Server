b3676985a1877b745bb2bea7ea0b2da97a94b22c
Artem <79054301+Bumer-32@users.noreply.github.com>
2024-09-28 22:42:55 +0300

    Add log levels

--- src/main/kotlin/ua/pp/lumivoid/iwtcms/util/ConfigData.kt
+++ src/main/kotlin/ua/pp/lumivoid/iwtcms/util/ConfigData.kt
@@ -3,4 +3,4 @@ package ua.pp.lumivoid.iwtcms.util
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ConfigData(val ip: String, val port: Int, val logLevel: Int)
+data class ConfigData(val ip: String, val port: Int, val logLevel: String)
