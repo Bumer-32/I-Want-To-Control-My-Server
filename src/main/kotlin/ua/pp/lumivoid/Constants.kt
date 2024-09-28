41913dfbdc441d9ad3c769a4f25c153ca1aa1602
Artem <krutog727@gmail.com>
2024-09-28 19:25:36 +0300

    Almost ready first version

---
+++ src/main/kotlin/ua/pp/lumivoid/Constants.kt
@@ -0,0 +1,8 @@
+package ua.pp.lumivoid
+
+import org.slf4j.LoggerFactory
+
+object Constants {
+    const val MOD_ID = "iwtcms"
+    val LOGGER = LoggerFactory.getLogger(MOD_ID)
+}
--- src/main/kotlin/ua/pp/lumivoid/IWTCMS.kt
+++ src/main/kotlin/ua/pp/lumivoid/IWTCMS.kt
@@ -1,12 +1,16 @@
 package ua.pp.lumivoid
 
 import net.fabricmc.api.ModInitializer
-import org.slf4j.LoggerFactory
+import ua.pp.lumivoid.server.StoppedServerTrigger
+import ua.pp.lumivoid.util.MinecraftServerStartedTrigger
 
 object IWTCMS : ModInitializer {
-    private val logger = LoggerFactory.getLogger("iwtcms")
+	private val logger = Constants.LOGGER
 
 	override fun onInitialize() {
 		logger.info("Hello from Bumer_32!")
+
+		MinecraftServerStartedTrigger.register()
+		StoppedServerTrigger.register()
 	}
 }
