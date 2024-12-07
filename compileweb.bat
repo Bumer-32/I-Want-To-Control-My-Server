e398b3c9403305a7ed040b04d42e48a9707e6cee
Artem <79054301+Bumer-32@users.noreply.github.com>
2024-12-07 21:31:17 +0200

    multi launch protection

--- compileweb.bat
+++ compileweb.bat
@@ -1,8 +1,14 @@
+@echo off
+
 REM This script added just for amenities in vscode (live server easier to launch in extern folder because typescript and sass needs compiling)
 REM Better to use it with this extention: https://marketplace.visualstudio.com/items?itemName=wk-j.save-and-run
 
-REM @echo off
 cd /d "%~dp0"
+
+REM check is webc exists (for multiple scripts launches)
+if exist webc\ (
+    exit /b
+)
 
 REM compile first in webc, because after compilation ts live server updates the page, and this occurs even BEFORE sass compilation, due to which the page remains without style
 xcopy src\main\web webc /E /I /Q /Y
