5af9c04279d1b3ca2092e4fbdfbec786c01ae4b3
Artem <79054301+Bumer-32@users.noreply.github.com>
2024-12-07 21:15:29 +0200

    now we can develop with ts and sass and even compile it

---
+++ compileweb.bat
@@ -0,0 +1,21 @@
+REM This script added just for amenities in vscode (live server easier to launch in extern folder because typescript and sass needs compiling)
+REM Better to use it with this extention: https://marketplace.visualstudio.com/items?itemName=wk-j.save-and-run
+
+REM @echo off
+cd /d "%~dp0"
+
+REM compile first in webc, because after compilation ts live server updates the page, and this occurs even BEFORE sass compilation, due to which the page remains without style
+xcopy src\main\web webc /E /I /Q /Y
+copy package-lock.json webc
+copy package.json webc
+cd webc
+call npm install
+call npx tsc --init
+call npx tsc --rootDir .
+call npx sass style.scss style.css
+
+cd ..
+rmdir /S /Q web
+xcopy webc web /E /I /Q /Y
+
+rmdir /S /Q webc
