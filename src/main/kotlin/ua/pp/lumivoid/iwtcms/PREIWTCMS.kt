693fbef7fc580eb4e03418a63bdaa307debd72c8
Artem <krutog727@gmail.com>
2025-01-07 00:17:26 +0200

    some user warnings

--- src/main/kotlin/ua/pp/lumivoid/iwtcms/PREIWTCMS.kt
+++ src/main/kotlin/ua/pp/lumivoid/iwtcms/PREIWTCMS.kt
@@ -5,15 +5,67 @@ import org.slf4j.LoggerFactory
 import ua.pp.lumivoid.iwtcms.ktor.KtorServer
 import ua.pp.lumivoid.iwtcms.util.Config
 import ua.pp.lumivoid.iwtcms.util.CustomLogger
-import kotlin.concurrent.thread
+import java.io.File
 
 object PREIWTCMS: PreLaunchEntrypoint {
     private val logger = LoggerFactory.getLogger("iwtcms pre launch")
-    @Suppress("DEPRECATION")
     override fun onPreLaunch() {
         logger.info("Initialize pre launch iwtcms")
+        checkForDevMode()
         CustomLogger.setup()
-
         KtorServer.setup()
+    }
+
+    private fun checkForDevMode() {
+        if (Config.readConfig().devMode) {
+            if (File("${Constants.CONFIG_FOLDER}/../../gradlew.bat").exists()) { // Check is mod launched in "developer environment"
+                logger.warn("")
+                logger.warn("")
+                logger.warn("")
+                logger.warn("")
+                logger.warn("")
+                logger.warn("+-----------------------------------------------------------------------------------------+")
+                logger.warn("|                                                                                         |")
+                logger.warn("|                       SEEMS LIKE YOU LAUNCHED IWTCMS IN DEV MODE                        |")
+                logger.warn("|                     BE CAREFUL, IT MAY CONTAIN BUGS AND BE UNSTABLE                     |")
+                logger.warn("|                                                                                         |")
+                logger.warn("|                IF YOU SEE THIS MESSAGE BUT DID NOT ENABLE DEV MODE, PLEASE              |")
+                logger.warn("|                          CONTACT THE DEVELOPERS ON GITHUB                               |")
+                logger.warn("|                                                                                         |")
+                logger.warn("|                          IWTCMS CARES ABOUT YOUR SECURITY!                              |")
+                logger.warn("|                                                                                         |")
+                logger.warn("+-----------------------------------------------------------------------------------------+")
+                logger.warn("")
+                logger.warn("")
+                logger.warn("")
+                logger.warn("")
+                logger.warn("")
+            } else {
+                logger.error("")
+                logger.error("")
+                logger.error("")
+                logger.error("")
+                logger.error("")
+                logger.error("+-----------------------------------------------------------------------------------------+")
+                logger.error("|                                                                                         |")
+                logger.error("|                     IT SEEMS YOU LAUNCHED IWTCMS IN DEV MODE                            |")
+                logger.error("|                            BUT YOU ARE NOT A DEVELOPER!                                 |")
+                logger.error("|            YOU WERE INSTRUCTED NOT TO ENABLE THIS OPTION IN THE CONFIG!                 |")
+                logger.error("|                                                                                         |")
+                logger.error("|                THE SERVER WILL NOT BE STARTED FOR SECURITY REASONS.                     |")
+                logger.error("|                  DEV MODE MAY INTRODUCE BUGS AND VULNERABILITIES.                       |")
+                logger.error("|                                                                                         |")
+                logger.error("|                      PLEASE DISABLE DEV MODE IN THE CONFIG!                             |")
+                logger.error("|                                                                                         |")
+                logger.error("|              IF YOU SEE THIS MESSAGE BUT DID NOT ENABLE DEV MODE, PLEASE                |")
+                logger.error("|                          CONTACT THE DEVELOPERS ON GITHUB                               |")
+                logger.error("|                                                                                         |")
+                logger.error("|                       IWTCMS CARES ABOUT YOUR SECURITY!                                 |")
+                logger.error("|                                                                                         |")
+                logger.error("+-----------------------------------------------------------------------------------------+")
+
+                System.exit(1)
+            }
+        }
     }
 }
