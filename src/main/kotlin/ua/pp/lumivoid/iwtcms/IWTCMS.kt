e72f5826e0af2afa4d8701c52af654f6a289f4a1
Artem <krutog727@gmail.com>
2025-02-22 15:18:06 +0200

    yaay! it runs!

--- src/main/kotlin/ua/pp/lumivoid/iwtcms/IWTCMS.kt
+++ src/main/kotlin/ua/pp/lumivoid/iwtcms/IWTCMS.kt
@@ -2,11 +2,11 @@ package ua.pp.lumivoid.iwtcms
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.loader.api.FabricLoader
-import ua.pp.lumivoid.iwtcms.ktor.api.dev.WebCompile
 import ua.pp.lumivoid.iwtcms.ktor.util.Config
 import ua.pp.lumivoid.iwtcms.util.StoppedServerTrigger
 import ua.pp.lumivoid.iwtcms.util.MinecraftServerHandler
 import java.awt.Desktop
+import java.lang.ProcessBuilder.Redirect
 import java.net.URI
 
 object IWTCMS : ModInitializer {
@@ -22,15 +22,35 @@ object IWTCMS : ModInitializer {
 			logger.info("Spark found!")
 		}
 
+		// ? run "npm run dev" if dev mode enabled
 		if (Config.readConfig().devMode) {
-			WebCompile.compileAll()
+			val runnerFile = if (System.getProperty("os.name").startsWith("Win")) {
+				"npmRunDev.bat"
+			} else {
+				"npmRunDev.sh"
+			}
+
+			val process = ProcessBuilder("${Constants.CONFIG_FOLDER}/../../$runnerFile")
+				.redirectOutput(Redirect.INHERIT)
+				.redirectError(Redirect.INHERIT)
+				.start()
+
+			// wait for shutdown
+			Runtime.getRuntime().addShutdownHook(Thread {
+				process.destroy()
+			})
 		}
 
 		if (Config.readConfig().autoOpenIWTCMSPageOnStartup) {
 			logger.info("Open IWTCMS page")
-			@Suppress("HttpUrlsUsage")
-			val prefix = if (Config.readConfig().useSSL) "https://" else "http://"
-			Desktop.getDesktop().browse(URI("$prefix${Config.readConfig().ip}:${Config.readConfig().port}"))
+			if (!Config.readConfig().devMode) {
+				@Suppress("HttpUrlsUsage")
+				val prefix = if (Config.readConfig().useSSL) "https://" else "http://"
+				Desktop.getDesktop().browse(URI("$prefix${Config.readConfig().ip}:${Config.readConfig().port}"))
+			} else {
+				@Suppress("HttpUrlsUsage")
+				Desktop.getDesktop().browse(URI("http://localhost:5173"))
+			}
 		}
 	}
 }
