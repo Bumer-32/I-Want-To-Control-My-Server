46487ab3871edec8aab7ba3dcf955a85cf2783dc
Artem <79054301+Bumer-32@users.noreply.github.com>
2025-02-22 14:12:27 +0200

    it runs on npm run dev!

--- src/web/App.svelte
+++ src/web/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
+  //import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
   <div>
     <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+<!--      <img src={viteLogo} class="logo" alt="Vite Logo" />-->
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
