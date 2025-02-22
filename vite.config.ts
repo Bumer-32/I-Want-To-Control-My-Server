a542df9624886f1c6fb2668c265ff3adbe6f77ce
Artem <krutog727@gmail.com>
2025-02-22 14:33:44 +0200

    oops wrong folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  root: "src/web",
+  root: "src/main/web",
 })
