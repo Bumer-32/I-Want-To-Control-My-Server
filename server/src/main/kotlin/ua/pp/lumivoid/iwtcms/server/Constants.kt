e98e9cb7d93d5ccdfdd929caf20e249682eabb69
Artem Samchuk <79054301+Bumer-32@users.noreply.github.com>
2026-01-28 21:01:25 +0200

    fixed

--- server/src/main/kotlin/ua/pp/lumivoid/iwtcms/server/Constants.kt
+++ server/src/main/kotlin/ua/pp/lumivoid/iwtcms/server/Constants.kt
@@ -12,6 +12,6 @@ internal object Constants {
     const val AVAILABLE_CONFIGS_SETTINGS_FILE = "/availableConfigs.yaml" // AVAILABLE_CONFIGS_SETTINGS_FILE
     const val NOT_FOUND_HTML = "/static/404.html"
 
-    val MOD_VERSION = javaClass.getResource("/iwtcms.version")!!.readText()
+    val MOD_VERSION = javaClass.getResource("/iwtcms.version")?.readText() ?: ""
     const val SCHEMA_VERSION = "v1"
 }
